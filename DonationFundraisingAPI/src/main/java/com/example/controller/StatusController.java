f59d8ba82a9308fbe4a56eacbb09d83faa84e256
LazarevskiGoce16 <lazarevskigoce26@gmail.com>
2025-05-29 15:14:49 +0200

    added json response for /status

--- DonationFundraisingAPI/src/main/java/com/example/controller/StatusController.java
+++ DonationFundraisingAPI/src/main/java/com/example/controller/StatusController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import com.example.dto.StatusDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/status")
 public class StatusController {
-    @GetMapping(produces = "text/plain")
-    public String getStatus() {
-        return "Donation and Fundraising API is Up and Running";
+    @GetMapping
+    public StatusDto getStatus() {
+        return new StatusDto("Donation and Fundraising API", "Up and Running");
     }
 }
